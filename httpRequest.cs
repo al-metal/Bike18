9e64569569aa7fbbb96d48abfe93db4540e78f25
al-metal <al-metal@bk.ru>
2016-12-01 14:59:54 +0400

    Закрыть соединение и без прокси

--- httpRequest.cs
+++ httpRequest.cs
@@ -14,11 +14,13 @@ namespace Bike18
         {
             HttpWebResponse res = null;
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Proxy = null;
             req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
             req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0";
             res = (HttpWebResponse)req.GetResponse();
             StreamReader ressr = new StreamReader(res.GetResponseStream(), Encoding.GetEncoding(1251));
             string otv = ressr.ReadToEnd();
+            res.Close();
             return otv;
         }
 
